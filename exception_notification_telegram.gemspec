06522c73ba9a9fc0aaa4bed20177bcf8e331e9a6
Ignacio Aguirrezabal <zirion0@gmail.com>
2019-02-19 23:36:03 -0300

    Add Telegram notifier

--- exception_notification_telegram.gemspec
+++ exception_notification_telegram.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "minitest", "~> 5.0"
 
   spec.add_dependency "exception_notification", "~> 4.3"
+  spec.add_dependency "httparty", "~> 0.10.2"
 end
--- lib/exception_notification_telegram.rb
+++ lib/exception_notification_telegram.rb
@@ -1,6 +1,24 @@
 require "exception_notification_telegram/version"
+require "HTTParty"
 
-module ExceptionNotificationTelegram
+module ExceptionNotifier
   class Error < StandardError; end
-  # Your code goes here...
+
+  class TelegramNotifier
+    def initialize(options)
+      @token = options.delete(:token)
+      @channel = options.delete(:channel)
+    end
+
+    def call(exception, options={})
+      message = if options[:accumulated_errors_count].to_i > 1
+                  "The exception occurred #{options[:accumulated_errors_count]} times: '#{exception.message}'"
+                else
+                  "A new exception occurred: '#{exception.message}'"
+                end
+      message += " on '#{exception.backtrace.first}'" if exception.backtrace
+
+      HTTParty.get("https://api.telegram.org/bot#{@token}/sendMessage?chat_id=#{@channel}&text=#{message}")
+    end
+  end
 end
